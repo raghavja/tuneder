23f9dbab1d79ca2a271eb3c98e2378d82c89b22f
Raghav Jain <raghavja@usc.edu>
2018-11-06 21:26:41 -0800

    added json parser

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -13,9 +13,15 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var fs = require("fs");
+var contents = fs.readFileSync("clientstuff.json");
+var jsonContent = JSON.parse(contents);
+
+console.log(jsonContent.client_id);
+
+var client_id = jsonContent.client_id; // Your client id
+var client_secret = jsonContent.client_secret; // Your secret
+var redirect_uri = jsonContent.redirect_uri; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
